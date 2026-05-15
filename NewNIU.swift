0f5fde6048c4536953082c5860227b9c68a65ddb
Papayuda <papayuda@duck.com>
2026-05-14 21:33:36 -0400

    Create NewNIU.swift

---
+++ NewNIU.swift
@@ -0,0 +1,16 @@
+//
+//  NewNIU.swift
+//  
+//
+//  Created by JH on 5/14/26.
+//
+
+import Testing
+
+struct NewNIU {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
